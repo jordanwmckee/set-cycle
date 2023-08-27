4ee0d63f2d915cb8f4f1584b211b9d41357cf84c
Jordan <jordanmckee@outlook.com>
2023-08-26 22:34:22 -0400

    set up project structure

---
+++ app/Sets App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Sets App
+//
+//  Created by Jordan McKee on 8/26/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ app/Sets App/Sets_App.swift
@@ -0,0 +1,17 @@
+//
+//  Sets_App.swift
+//  Sets App
+//
+//  Created by Jordan McKee on 8/26/23.
+//
+
+import SwiftUI
+
+@main
+struct Sets_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
