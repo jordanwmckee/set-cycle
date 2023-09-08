2232b655cd0baf65c902123f61b3f8715328dd91
Jordan <jordanmckee@outlook.com>
2023-09-08 18:39:13 -0400

    update: fix dockerfile for development hot reloading

--- server/app/docker/docker-entrypoint.sh
+++ server/app/docker/docker-entrypoint.sh
@@ -8,5 +8,14 @@ if [ -n "$DB_HOST" ]; then
   ./docker/wait-for-it.sh "$DB_HOST:${DB_PORT:-3306}"
 fi
 
+# set up github.com/codegangsta/gin for live reloading
+if [ "$GIN_MODE" = "release" ]; then
+  echo "Running in release mode"
+else
+  echo "Running in development mode"
+  go install github.com/codegangsta/gin@latest
+  go mod tidy
+fi
+
 # Run the main container command.
 exec "$@"
